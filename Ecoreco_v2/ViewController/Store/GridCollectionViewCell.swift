f2bee056ea22852c93c2f85d7d066a24eab423fd
Ma-Hyekjin <a01080077504@gmail.com>
2022-10-31 15:59:16 +0900

    22.10.31.16

---
+++ Ecoreco_v2/ViewController/Store/GridCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GirdCollectionViewCell.swift
+//  Ecoreco_v2
+//
+//  Created by 마혁진 on 2022/10/31.
+//
+
+import UIKit
+
+class GridCollectionViewCell: UICollectionViewCell {
+    
+}
--- Ecoreco_v2/ViewController/Store/StoreViewController.swift
+++ Ecoreco_v2/ViewController/Store/StoreViewController.swift
@@ -23,7 +23,6 @@ class StoreViewController: UIViewController {
         //장보기창 이동 or 팝업 띄우기
     }
     
-    
     @IBOutlet weak var skinCare: UIButton!
     @IBAction func skinCareButton(_ sender: UIButton) {
         if self.count3 == true {
@@ -168,6 +167,10 @@ class StoreViewController: UIViewController {
     }
     
     
+//    @IBOutlet weak var collectionView: UICollectionView!
+    
+//    var arrImageSet: [String] = ["image1", "image2", "image3", "image4", "image5", "image6"]
+
     @IBOutlet weak var searchBar: UISearchBar!
     
     
@@ -178,3 +181,55 @@ class StoreViewController: UIViewController {
     }
     
 }
+
+//extension StoreViewController: UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+//    // CollectionView item 개수
+//        func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//            return arrImageSet.count
+//        }
+//
+//        // CollectionView Cell의 Object
+//        func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCollectionViewCell", for: indexPath) as! GridCollectionViewCell
+//
+//            cell.image.image = UIImage(named: arrImageSet[indexPath.row]) ?? UIImage()
+//
+//            return cell
+//        }
+//
+//        // CollectionView Cell의 Size
+//        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+//
+//            let width: CGFloat = collectionView.frame.width / 3 - 1.0
+//
+//            return CGSize(width: width, height: width)
+//        }
+//
+//        // CollectionView Cell의 위아래 간격
+//        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+//            return 1.0
+//        }
+//
+//        // CollectionView Cell의 옆 간격
+//        func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+//            return 1.0
+//        }
+//}
+//
+//extension StoreViewController: UICollectionViewDataSource, UICollectionViewDelegate{
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        return 6
+//    }
+//
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as?
+//                    UICollectionViewCell else {
+//                return UICollectionViewCell()
+//            }
+//
+//            return cell
+//    }
+//
+//
+//
+//}
